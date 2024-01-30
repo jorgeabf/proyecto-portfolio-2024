7a89b2ca94aa54aab9578dac0696e62b9500363f
jorgeabf <aportuvas@outlook.es>
2024-01-30 15:49:35 +0100

    Update Header and Layout components

---
+++ src/const.ts
@@ -0,0 +1,6 @@
+const SITE_TITLE: string = 'Jorge Arce Web';
+const SITE_DESCRIPTION: string = 'Portfolio de Jorge Arce, Desarrollador Web Full Stack';
+const SITE_URL: string = 'https://jorgearce.dev';
+const ACTUAL_YEAR: number = new Date().getFullYear();
+
+export { SITE_TITLE, SITE_DESCRIPTION, SITE_URL, ACTUAL_YEAR };
