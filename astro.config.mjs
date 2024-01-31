2aeab3be0af02cb8acac8ce92e58471f1df201c3
jorgeabf <aportuvas@outlook.es>
2024-01-31 16:01:00 +0100

    Remove astro-icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
-import icon from 'astro-icon'
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind()],
   site: 'https://www.jorgearce.dev'
 })
